3484e11921e10d210ee369912fceb06d82c01ec5
UpAndDownAgain <musil.adam@gmail.com>
2020-01-23 22:23:04 +0100

    anotace v movie

--- Filmoteka/Models/Movie.cs
+++ Filmoteka/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,16 @@ namespace Filmoteka.Models
     public class Movie
     {
         public int id { get; set; }
+
+        [Display(Name = "Název")]
         public string title { get; set; }
+
+        [Display(Name = "Datum vydání")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime releaseDate { get; set; }
+        
+        [Display(Name = "Žánr")]
         public string genre { get; set; }
         public decimal price { get; set; }
     }

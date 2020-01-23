2c880d4e596df46879775bbc234d61da5a30a5ce
UpAndDownAgain <musil.adam@gmail.com>
2020-01-23 17:42:57 +0100

    helloworld controler

---
+++ Filmoteka/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Filmoteka.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is my default action";
+        }
+
+        //GET: /HelloWorld/Welcolme
+        public string Welcome(string n, int i = 0)
+        {
+            return HttpUtility.HtmlEncode("Hello " + n + ", Number is: " + i);
+        }
+    }
+}
